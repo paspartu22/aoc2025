ba6ae78167db129f21266cd36243c2cb621b4ec8
Andrey Ovodov <andreyovodov@yandex.ru>
2025-12-02 10:36:37 +0300

    day2 p2

--- day02/main.py
+++ day02/main.py
@@ -1,5 +1,37 @@
-def part1(name):
+def check_pattern(number, pattern):
+    pattern_len = len(pattern)
+    if (len(number)/pattern_len != len(number)//pattern_len):
+        return 0
+    for i in range(1, len(number)//pattern_len):
+        # print(number[pattern_len*i:pattern_len*(i+1)])
+        if number[pattern_len*i:pattern_len*(i+1)] != pattern:
+            return 0
+    print(f'FOUND {number}')
+    return 1
 
+def part2(name):
+    with open(name) as file:
+        result_list = []
+        result = 0
+        lines = file.readlines()
+        ranges = lines[0].split(',')
+        for one_range in ranges:
+            # print(one_range)
+            start, finish = one_range.split('-')
+            for i in range(int(start), int(finish)+1):
+                # print(f'number {i}')
+                number = str(i)
+                for j in range(1,len(number)//2+1):
+                    # print(j)
+                    pattern = number[:j]
+                    # print(f'pattern {pattern}')
+                    if (check_pattern(number, pattern)):
+                        result_list.append(i)
+                        result += i
+                        break
+    return result
+
+def part1(name):
     with open(name) as file:
         result = 0
         lines = file.readlines()
@@ -10,16 +42,19 @@ def part1(name):
             for i in range(int(start), int(finish)+1):
                 # print(i)
                 number = str(i)
-                # print(number[:len(number)//2])
-                # print(number[len(number)//2:])
                 if (number[:len(number)//2] == number[len(number)//2:]):
                     result += i
     return result
+
 
 def main():
     print("should be 1227775554")
     print(part1('day02/test.txt'))
     print(part1('day02/data.txt'))
+
+    print("should be 4174379265")
+    print(part2('day02/test.txt'))
+    print(part2('day02/data.txt'))
 
 if __name__ == "__main__":
     main()
